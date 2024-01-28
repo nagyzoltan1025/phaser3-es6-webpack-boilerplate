4b959acbe0f7a64d670a7334501f33ba2b8c413c
nagyzoltan1025 <36644322+nagyzoltan1025@users.noreply.github.com>
2024-01-28 19:13:01 +0100

    Create Animation

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,10 +9,24 @@ class Game extends Phaser.Scene {
 
   preload() {
     this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight:64
+    });
   }
 
   create(data) {
-    this.add.image(400, 300, 'logo');
+    this.anims.create({
+      key: 'hero-running',
+      frames: this.anims.generateFrameNumbers('hero-run-sheet'),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.player = this.add.sprite(400, 300, 'hero-run-sheet');
+    this.player.anims.play('hero-running')
+
+
   }
 
   update(time, delta) {}
