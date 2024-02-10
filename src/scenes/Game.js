8e4f84a26a5b5d15db41fb0fcc9874a3078c2918
nagyzoltan1025 <36644322+nagyzoltan1025@users.noreply.github.com>
2024-02-10 22:39:45 +0100

    Add new animations

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,6 +9,31 @@ class Game extends Phaser.Scene {
   init(data) {}
 
   preload() {
+    this.load.spritesheet('hero-idle-sheet', 'assets/hero/idle.png', {
+      frameWidth: 32,
+      frameHeight:64
+    });
+
+    this.load.spritesheet('hero-pivot-sheet', 'assets/hero/pivot.png', {
+      frameWidth: 32,
+      frameHeight:64
+    });
+
+    this.load.spritesheet('hero-jump-sheet', 'assets/hero/jump.png', {
+      frameWidth: 32,
+      frameHeight:64
+    });
+
+    this.load.spritesheet('hero-flip-sheet', 'assets/hero/spinjump.png', {
+      frameWidth: 32,
+      frameHeight:64
+    });
+
+    this.load.spritesheet('hero-fall-sheet', 'assets/hero/fall.png', {
+      frameWidth: 32,
+      frameHeight:64
+    });
+
     this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
       frameWidth: 32,
       frameHeight:64
@@ -22,6 +47,37 @@ class Game extends Phaser.Scene {
     this.anims.create({
       key: 'hero-running',
       frames: this.anims.generateFrameNumbers('hero-run-sheet'),
+    });
+
+    this.anims.create({
+      key: 'hero-idle',
+      frames: this.anims.generateFrameNumbers('hero-idle-sheet'),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'hero-pivoting',
+      frames: this.anims.generateFrameNumbers('hero-pivot-sheet'),
+    });
+
+    this.anims.create({
+      key: 'hero-jumping',
+      frames: this.anims.generateFrameNumbers('hero-jump-sheet'),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'hero-flipping',
+      frames: this.anims.generateFrameNumbers('hero-flip-sheet'),
+      frameRate: 30,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: 'hero-falling',
+      frames: this.anims.generateFrameNumbers('hero-fall-sheet'),
       frameRate: 10,
       repeat: -1
     });
